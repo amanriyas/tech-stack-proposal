cfca5ea833cb5b89de540720f3f68d69fc1ab9da
amanriyas <153854025+amanriyas@users.noreply.github.com>
2025-01-31 23:05:00 +0400

    testing

--- student/views.py
+++ student/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 # Create your views here.
 
-class Index(APIView):
+clas Index(APIView):
 
     def get(self,request):
         return Response({"message": "Hello from Django!"})
