fcd8f70b0c843a3c46efa77bf505d20445e779eb
amanriyas <153854025+amanriyas@users.noreply.github.com>
2025-01-31 19:08:44 +0400

    updated dependencies

--- frontend/job_board/src/App.jsx
+++ frontend/job_board/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Routes,Route } from 'react-router-dom'
 
 function App() {
   const [data, setData] = useState(null);
