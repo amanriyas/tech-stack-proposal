beb4a04347359ff1efa274d281b5d247f328e6a7
amanriyas <153854025+amanriyas@users.noreply.github.com>
2025-02-07 17:24:58 +0400

    added cd

--- student/models.py
+++ student/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# #models = database tables
+# # Create your models here.
+
+# class Student(models.Model):
+#     name = models.CharField(max_length = 200)
+#     age = models.IntegerField()
+#     registered_on = models.DateTimeField(auto_now_add=True)
+#     comments = models.CharField(blank= True)
+
+#     class Meta:
+#       db_table = 'Student'  
+
+# class club_leaders(models.Model):
+#    name = models.ForeignKey(Student, on_delete= models.CASCADE)      
+
+
